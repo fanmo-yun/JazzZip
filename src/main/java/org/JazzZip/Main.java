31676f9ca7e833b0d4579dc8de66e74ea7e7bd33
fanmo-yun <xiaomojun9527@outlook.com>
2024-02-09 23:04:45 +0800

    JazzZip update

--- src/main/java/org/JazzZip/Main.java
+++ src/main/java/org/JazzZip/Main.java
@@ -35,9 +35,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            deleteContents(new File(System.getProperty("user.dir") + "\\temp"));
-        }));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> deleteContents(new File(System.getProperty("user.dir") + "\\temp"))));
 
         CheckTempPathExists();
         MainWin.MainWindow();
